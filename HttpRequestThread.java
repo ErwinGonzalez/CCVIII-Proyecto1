2c0a2150a7192423a0b967755eeffd77805c3f4a
ErwinGonzalez <erwinggonzalez@galileo.edu>
2019-08-09 16:52:04 -0600

    simplificado el get, deberia aceptar cualquier formato de url bien construido

--- HttpRequestThread.java
+++ HttpRequestThread.java
@@ -6,8 +6,9 @@ class HttpRequestThread implements Runnable {
     private final static Logger logger = Logger.getLogger(ServerThread.class.getName());
     SimpleDateFormat format = new SimpleDateFormat("EEE dd/MM/yyyy hh:mm:ss z");
 
-    /**Este constructor, posiblemente podria recibir la pagina, en lugar de 
-     * tenerla hardccoded en el metodo run
+    /**
+     * Este constructor, posiblemente podria recibir la pagina, en lugar de tenerla
+     * hardccoded en el metodo run
      */
     public HttpRequestThread() {
 
@@ -17,8 +18,8 @@ class HttpRequestThread implements Runnable {
     public void run() {
 
         try {
-            /**Importante: No agregar el "http://" al url */
-            sendGet("www.galileo.edu");
+
+            sendGet("www.galileo.edu/fisicc/");
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
@@ -26,71 +27,65 @@ class HttpRequestThread implements Runnable {
 
     private void sendGet(String url) throws Exception {
 
-        /*TODO hacer un ciclo, empezar a leer los puertos, leer la primera linea de cada respuesta
-          nos interesa encontrar las respuestas que nos den codigo 200 (OK), 301 (Moved Temporarily) o 302(Moved Permantentely)
-        */
+        /*
+         * TODO hacer un ciclo, empezar a leer los puertos, leer la primera linea de
+         * cada respuesta nos interesa encontrar las respuestas que nos den codigo 200
+         * (OK), 301 (Moved Temporarily) o 302(Moved Permantentely)
+         */
 
-        /** TODO si se manda un url, con una pagina especifica, por ejemplo galileo.edu/fisicc
-         * hay que separar el url de domino de la pagina 
-         * y cambiar la estructura del get*/
-        String page = "/fisicc";
 
-        
-        SocketUtil socketUtil = new SocketUtil(url,80);
-        socketUtil.sendGet(url, page);
+        SocketUtil socketUtil = new SocketUtil(url, 80);
+        socketUtil.sendGet(url);
         String str;
-        while((str = socketUtil.readOneLine())!=null)
+        while ((str = socketUtil.readOneLine()) != null)
+            //TODO esta leyendo un 0/null de mas
             System.out.println(str);
-            
-        //System.out.println(socketUtil.isResponse200());
-        /*Socket socket = new Socket(url ,80);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))); 
-        out.println(message);
-        out.println("\r\n\r\n");
-         
-        out.flush(); 
-        String str;
-        while((str= in.readLine())!=null){
-                
-                System.out.println(str);
-               
-        }
-        socket.close();*/
-        
-        /* TODO Este codigo iria dentro del loop, para buscar las respuestas
-         * De obtener un 301 0 302 hay que buscar como manejar el redirect
+
+        // System.out.println(socketUtil.isResponse200());
+        /*
+         * Socket socket = new Socket(url ,80); BufferedReader in = new
+         * BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter
+         * out = new PrintWriter(new BufferedWriter(new
+         * OutputStreamWriter(socket.getOutputStream()))); out.println(message);
+         * out.println("\r\n\r\n");
+         * 
+         * out.flush(); String str; while((str= in.readLine())!=null){
+         * 
+         * System.out.println(str);
+         * 
+         * } socket.close();
          */
 
-        /*Ciclo de lectura del input de la pagina, hay que mejorarlo
-         * posiblemente seria buena idea guardar este resultado en un archivo (ej. temp.txt) 
-         * para tener donde leer/manipular el resultado
-         */
-        
-       /**Una vez que se tenga el resultado, hay que usar jsoup para crear una lista de tags
-        * leer por bloques para poder dar forma al arbol
-        * ej.
-        *   for(getAllh1)
-        *     getAllahref
-        *           ...
-        * asi, leyendo por tablas/clases, etc
-        * aqui mismo, podemos aprovechar a leer la metadata de cada elemento (direccion del salto, nombres de clases, descripciones, etc)
-        * eso lo guardamos en una lista, posiblemente un mapa o un hash para poder referenciar de vuelta con la clase
-        */
-
-        /** Esta lista, hay que revisar los contenidos, viendo que ,por ejemplo
-         * el index, solo deberia salir una vez en el arbol, aun si hay varios saltos al index
+        /*
+         * TODO Este codigo iria dentro del loop, para buscar las respuestas De obtener
+         * un 301 0 302 hay que buscar como manejar el redirect
          */
 
-        /** Luego se deberia crear un html de respuesta
-         * dos columnas, la columna izquierda tendria dos filas
+        /*
+         * Ciclo de lectura del input de la pagina, hay que mejorarlo posiblemente seria
+         * buena idea guardar este resultado en un archivo (ej. temp.txt) para tener
+         * donde leer/manipular el resultado
+         */
+
+        /**
+         * Una vez que se tenga el resultado, hay que usar jsoup para crear una lista de
+         * tags leer por bloques para poder dar forma al arbol ej. for(getAllh1)
+         * getAllahref ... asi, leyendo por tablas/clases, etc aqui mismo, podemos
+         * aprovechar a leer la metadata de cada elemento (direccion del salto, nombres
+         * de clases, descripciones, etc) eso lo guardamos en una lista, posiblemente un
+         * mapa o un hash para poder referenciar de vuelta con la clase
+         */
+
+        /**
+         * Esta lista, hay que revisar los contenidos, viendo que ,por ejemplo el index,
+         * solo deberia salir una vez en el arbol, aun si hay varios saltos al index
+         */
+
+        /**
+         * Luego se deberia crear un html de respuesta dos columnas, la columna
+         * izquierda tendria dos filas
          ***********************************
-         *          *                      *
-         *          *                      *  
-         * Arbol    *    Pagina Web        *
-         *          *                      * 
-         *          ************************ 
-         *          *       Metadata       * 
+         * * * * * Arbol * Pagina Web * * * ************************ * Metadata *
          ***********************************
          */
     }
--- SocketUtil.java
+++ SocketUtil.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-class SocketUtil{
+class SocketUtil {
     Socket socket;
     BufferedReader socketInputStream;
     PrintWriter socketOutputStream;
@@ -19,40 +19,37 @@ class SocketUtil{
     String domain;
     String page;
     int port;
-    public SocketUtil(String url, int port) throws IOException{
+
+    public SocketUtil(String url, int port) throws IOException {
         this.port = port;
-       resetSocket(url,port);
+        resetSocket(url, port);
     }
 
-    public void sendGet(String domain, String page)throws IOException {
-        this.domain = domain;
-        this.page = page;
-        String message = "GET "+page+" HTTP/1.1\r\n"+
-                         "Host: "+domain+"\r\n"+
-                         "Accept: */*\r\n"+
-                         "Connection: keep-alive\r\n"+
-                         "User-Agent: Mozilla/5.0\r\n";
+    public void sendGet(String url) throws IOException {
+        this.domain = getDomain(url);
+        this.page = getSite(url);
+        System.out.println("domain "+domain+"site "+page);
+        String message = "GET " + page + " HTTP/1.1\r\n" + "Host: " + domain + "\r\n" + "Accept: */*\r\n"
+                + "Connection: keep-alive\r\n" + "User-Agent: Mozilla/5.0\r\n";
         socketOutputStream.println(message);
-        socketOutputStream.println(headerSeparator);     
+        socketOutputStream.println(headerSeparator);
         socketOutputStream.flush();
-        
-                 int responseCode = getResponseCode();
-        if(responseCode == 301 || responseCode == 302)
+        int responseCode = getResponseCode();
+        if (responseCode == 301 || responseCode == 302)
             getForwardAddress();
 
-            
-
-           
     }
 
-    public String readOneLine() throws IOException{
-        if((lastReadMessage= socketInputStream.readLine())!=null){
-            
-                fullReadMessage.add(lastReadMessage);
-            
+    public String readOneLine() throws IOException {
+        if ((lastReadMessage = socketInputStream.readLine()) != null) {
+
+            fullReadMessage.add(lastReadMessage);
+
             return lastReadMessage;
+        } else {
+            return null;
         }
-        return null;
+
     }
 
     public int getResponseCode() throws IOException {
@@ -60,83 +57,90 @@ class SocketUtil{
         readOneLine();
         socketInputStream.reset();
         String[] requestLine = lastReadMessage.split(" ");
-       // System.out.println(lastReadMessage);
+        // System.out.println(lastReadMessage);
         return Integer.parseInt(requestLine[1]);
     }
 
     public void getForwardAddress() throws IOException {
         boolean finished = false;
-        String[] requestLine= lastReadMessage.split(" ");
-        while(!finished){
-            if(requestLine[0].equals("Location:"))
+        String[] requestLine = lastReadMessage.split(" ");
+        while (!finished) {
+            if (requestLine[0].equals("Location:"))
                 finished = true;
-            else{
+            else {
                 readOneLine();
                 requestLine = lastReadMessage.split(" ");
             }
         }
-       // String[] redirectPage = requestLine[1].split("/");
-       /* System.out.println(requestLine[1]);
-        for(String part: redirectPage)
-            System.out.println(part);*/
+        // String[] redirectPage = requestLine[1].split("/");
+        /*
+         * System.out.println(requestLine[1]); for(String part: redirectPage)
+         * System.out.println(part);
+         */
 
-        /*if(redirectPage.length == 4)
-            this.page = redirectPage[3];
-        this.domain = redirectPage[2];*/
+        /*
+         * if(redirectPage.length == 4) this.page = redirectPage[3]; this.domain =
+         * redirectPage[2];
+         */
 
-        
-        //TODO get the correct path, splitting on "/" seems not appropiate
-        String site = removeHttp(requestLine[1]);
-        resetSocket(getDomain(site),port);
-       // System.out.println(requestLine[1]);
-       // System.out.println(site+"\n"+getDomain(site)+"\n"+getSite(site));
-        sendGet(getDomain(site),getSite(site));
-        
-        //TODO make the new GET request
+        // TODO get the correct path, splitting on "/" seems not appropiate
+        resetSocket(getDomain(requestLine[1]), port);
+     
+        // System.out.println(requestLine[1]);
+        // System.out.println(site+"\n"+getDomain(site)+"\n"+getSite(site));
+        sendGet(requestLine[1]);
+      
+        // TODO make the new GET request
     }
 
-    public String removeHttp(String url){
-        String ret="";
-        if(url.contains("http://"))
-            ret = url.replace("http://","");
-        if(url.contains("https://"))
-            ret = url.replace("https://","");
+    public String removeHttp(String url) {
+        String ret = url;
+        if (url.contains("http://"))
+            ret = url.replace("http://", "");
+        if (url.contains("https://"))
+            ret = url.replace("https://", "");
         return ret;
     }
-    public String getDomain(String url){
-        return url.substring(0,url.indexOf("/"));
+
+    public String getDomain(String url) {
+        String site = removeHttp(url);
+        return site.contains("/") ? site.substring(0, site.indexOf("/")):site;
     }
-    public String getSite(String url){
-        return url.substring(url.indexOf("/"),url.length());
+
+    public String getSite(String url) {
+        String site = removeHttp(url);
+        return site.contains("/") ? site.substring(site.indexOf("/"),site.length()) : "/";
+        
     }
-    public void resetSocket(String url, int port) throws IOException{
-        try{
-            socket = new Socket(url, port);
+
+    public void resetSocket(String url, int port) throws IOException {
+        try {
+            socket = new Socket(getDomain(url), port);
             socketInputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            socketOutputStream = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))); 
-        }catch(UnknownHostException uhe){
+            socketOutputStream = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+        } catch (UnknownHostException uhe) {
             uhe.printStackTrace();
-        }
-        catch(IOException ioe){
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
         fullReadMessage = new ArrayList<>();
     }
-    
-    /*TODO readAll doesn't work propertly, bug, returns the sent message, not the inputStream
-     *Do not use
+
+    /*
+     * TODO readAll doesn't work propertly, bug, returns the sent message, not the
+     * inputStream Do not use
      */
-    public  ArrayList<String> readAll() throws IOException{
-        while((lastReadMessage = readOneLine()) != null)
+    public ArrayList<String> readAll() throws IOException {
+        while ((lastReadMessage = readOneLine()) != null)
             fullReadMessage.add(lastReadMessage);
 
         return fullReadMessage;
     }
-    public void CloseSocket() throws IOException{
+
+    public void CloseSocket() throws IOException {
         socket.close();
         socketInputStream.close();
         socketOutputStream.close();
     }
-
 
 }
