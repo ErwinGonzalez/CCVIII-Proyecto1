f279106e7ff97034c1c505dc44feb84d7dd8bb17
ErwinGonzalez <erwinggonzalez@galileo.edu>
2019-08-08 21:37:58 -0600

    finished redirecting

--- HttpRequestThread.java
+++ HttpRequestThread.java
@@ -1,14 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.util.logging.Logger;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.logging.Logger;
 
 class HttpRequestThread implements Runnable {
 
--- SocketUtil.java
+++ SocketUtil.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -20,26 +19,15 @@ class SocketUtil{
     String domain;
     String page;
     int port;
-    public SocketUtil(String url, int port){
+    public SocketUtil(String url, int port) throws IOException{
         this.port = port;
-        try{
-            //TODO check if the URL is trying to access a page other than the index, split host and page
-            socket = new Socket(url,port);
-            socketInputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            socketOutputStream = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))); 
-        }catch(UnknownHostException uhe){
-            uhe.printStackTrace();
-        }
-        catch(IOException ioe){
-            ioe.printStackTrace();
-        }
-        fullReadMessage = new ArrayList<>();
+       resetSocket(url,port);
     }
 
     public void sendGet(String domain, String page)throws IOException {
         this.domain = domain;
         this.page = page;
-        String message = "GET "+page+" HTTP/1.0\r\n"+
+        String message = "GET "+page+" HTTP/1.1\r\n"+
                          "Host: "+domain+"\r\n"+
                          "Accept: */*\r\n"+
                          "Connection: keep-alive\r\n"+
@@ -47,15 +35,14 @@ class SocketUtil{
         socketOutputStream.println(message);
         socketOutputStream.println(headerSeparator);     
         socketOutputStream.flush();
-        int responseCode = getResponseCode();
+        
+                 int responseCode = getResponseCode();
         if(responseCode == 301 || responseCode == 302)
             getForwardAddress();
 
             
 
-            for(String line: fullReadMessage)        {
-                System.out.println(line);
-            }
+           
     }
 
     public String readOneLine() throws IOException{
@@ -69,9 +56,11 @@ class SocketUtil{
     }
 
     public int getResponseCode() throws IOException {
+        socketInputStream.mark(1000);
         readOneLine();
+        socketInputStream.reset();
         String[] requestLine = lastReadMessage.split(" ");
-        System.out.println(lastReadMessage);
+       // System.out.println(lastReadMessage);
         return Integer.parseInt(requestLine[1]);
     }
 
@@ -81,21 +70,48 @@ class SocketUtil{
         while(!finished){
             if(requestLine[0].equals("Location:"))
                 finished = true;
-            else
+            else{
                 readOneLine();
-            requestLine = lastReadMessage.split(" ");
+                requestLine = lastReadMessage.split(" ");
+            }
         }
-        String[] redirectPage = requestLine[1].split("/");
-        System.out.println(requestLine[1]);
+       // String[] redirectPage = requestLine[1].split("/");
+       /* System.out.println(requestLine[1]);
         for(String part: redirectPage)
-            System.out.println(part);
+            System.out.println(part);*/
 
         /*if(redirectPage.length == 4)
             this.page = redirectPage[3];
         this.domain = redirectPage[2];*/
 
+        
+        //TODO get the correct path, splitting on "/" seems not appropiate
+        String site = removeHttp(requestLine[1]);
+        resetSocket(getDomain(site),port);
+       // System.out.println(requestLine[1]);
+       // System.out.println(site+"\n"+getDomain(site)+"\n"+getSite(site));
+        sendGet(getDomain(site),getSite(site));
+        
+        //TODO make the new GET request
+    }
+
+    public String removeHttp(String url){
+        String ret="";
+        if(url.contains("http://"))
+            ret = url.replace("http://","");
+        if(url.contains("https://"))
+            ret = url.replace("https://","");
+        return ret;
+    }
+    public String getDomain(String url){
+        return url.substring(0,url.indexOf("/"));
+    }
+    public String getSite(String url){
+        return url.substring(url.indexOf("/"),url.length());
+    }
+    public void resetSocket(String url, int port) throws IOException{
         try{
-            socket = new Socket(redirectPage[2],port);
+            socket = new Socket(url, port);
             socketInputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             socketOutputStream = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))); 
         }catch(UnknownHostException uhe){
@@ -105,20 +121,15 @@ class SocketUtil{
             ioe.printStackTrace();
         }
         fullReadMessage = new ArrayList<>();
-        //TODO get the correct path, splitting on "/" seems not appropiate
-        sendGet(redirectPage[2],"/"+redirectPage[3]+"/");
-        
-        //TODO make the new GET request
     }
     
     /*TODO readAll doesn't work propertly, bug, returns the sent message, not the inputStream
      *Do not use
      */
-    public ArrayList<String> readAll() throws IOException{
-        if(lastReadMessage != null)
-            fullReadMessage.add(lastReadMessage);
+    public  ArrayList<String> readAll() throws IOException{
         while((lastReadMessage = readOneLine()) != null)
             fullReadMessage.add(lastReadMessage);
+
         return fullReadMessage;
     }
     public void CloseSocket() throws IOException{
