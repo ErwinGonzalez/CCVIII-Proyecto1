318ac6392ed7f4b0cdc1e109059735ea579184b0
ErwinGonzalez <erwinggonzalez@galileo.edu>
2019-08-20 11:33:08 -0600

    added class show/hide

--- htmlOnClickTesting.java
+++ htmlOnClickTesting.java
@@ -19,6 +19,25 @@ public class htmlOnClickTesting implements Runnable{
     private DataOutputStream dos;
 
     ArrayList<UrlItem> urlList = new ArrayList<UrlItem>();
+    private final String style = "<style>\n.hidden{\ndisplay : none;}\n" +
+                                ".shown{\nbackground-color : #9E9E9E;}\n" +
+                                ".col1{width : 60%;}\n" +
+                                ".col2{width : 40%;}  </style>";
+    private final String script = "<script type= \"text/javascript\"\n>"+
+            "function testFunction(url){" +
+            "var cName = url+\"_html\";\n"+
+
+            "var y = document.getElementsByClassName(\"shown\");"+
+            "var i;"+
+            "for(i = 0; i<y.length;i++){" +
+            "y[i].classList.add(\"hidden\");" +
+            "y[i].classList.remove(\"shown\");}" +
+
+            "var x = document.getElementById(cName);\n" +
+            "x.classList.remove(\"hidden\");" +
+            "x.classList.add(\"shown\");}" +
+            "</script>";
+
 
     public htmlOnClickTesting(Socket socket){
         this.connection = socket;
@@ -33,7 +52,8 @@ public class htmlOnClickTesting implements Runnable{
 
     public String readAllUrls(UrlItem rootItem){
         String returnString  = "<ul>";
-        returnString += "<li>"+"<a href=javascript:testFunction(\""+rootItem.url+"\");>View HTML</a>"+rootItem.url+"</li>";
+        returnString += "<li>"+"<a href=javascript:testFunction(\""+rootItem.url+"\");>View HTML</a>" +
+                "<a href=\"http://www.google.com\" target=\"_blank\">"+rootItem.url+"</a></li>";
         for (UrlItem childItem : rootItem.childUrls){
             //returnString+="<ul><li>"+childItem.url+"</li>";
             returnString += readAllUrls(childItem);
@@ -43,7 +63,7 @@ public class htmlOnClickTesting implements Runnable{
         return returnString;
     }
     public String readAllHtml(UrlItem rootItem){
-        String returnString  = "<div id="+rootItem.url+"_html>"+rootItem.url+"<br>"+rootItem.html+"</div>";
+        String returnString  = "<div id="+rootItem.url+"_html class=\"hidden\">"+rootItem.url+"<br>"+rootItem.html+"</div>";
         for(UrlItem child: rootItem.childUrls)
            returnString += readAllHtml(child);
         return returnString;
@@ -59,14 +79,8 @@ public class htmlOnClickTesting implements Runnable{
 
 
         String content = "<html>"+
-                "<head><script type= \"text/javascript\">\nfunction testFunction(url)"+"" +
-                "{" +
-                "var cName = url+\"_html\";\n"+
-                "alert(cName);"+
-                "var x = document.getElementById(cName);\n" +
-                "x.style.display = \"none\";}\n" +
-                "</script></head>"+
-                "<body><table style=\"width:100%\"><tr><th>Urls</th><th>html</th></tr></tr><td>"+readAllUrls(root)+"</td>"+"<td>"+readAllHtml(root)+"</td></tr></table>"+
+                "<head>"+style+script+"</head>"+
+                "<body><table style=\"width:100%\"><tr><th class = \"col1\">Urls</th><th class=\"col2\">html</th></tr></tr><td>"+readAllUrls(root)+"</td>"+"<td>"+readAllHtml(root)+"</td></tr></table>"+
 
                 "</body></html>";
         String htmlResponse = "HTTP/1.1 200 OK\n"+
